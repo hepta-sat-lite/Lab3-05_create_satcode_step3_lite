07d38821707c684e75872dfc7449912019eabfa8
Charlie.com <csru22005@g.nihon-u.ac.jp>
2023-06-05 08:16:10 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include "mbed.h"
-
-DigitalOut myled(LED1);
-
+#include "HEPTA_EPS.h"
+HEPTA_EPS eps(PA_0,PA_4);
+Serial pc(USBTX,USBRX,9600);
 int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+    int flag = 0; // condition
+    float btvol;
+    for(int i = 0; i < 100; i++) {
+        //Get Battery Voltage
+        eps.vol(&btvol);
+        pc.printf("BatVol = %.2f [V]\r\n",btvol);
+        //Power Saving Mode
+        if(btvol <= 3.5) {
+            eps.shut_down_regulator();
+            flag = 1;
+        } else {
+            eps.turn_on_regulator();
+            flag = 0;
+        }
+        if(flag == 1) {
+            pc.printf("Power saving mode ON\r\n"); 
+        }
+        wait(1.0);
+    }
 }
