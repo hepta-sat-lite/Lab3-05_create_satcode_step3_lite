e2799913dadb2a0469d1659030f44cb78803547c
Charlie.com <csru22005@g.nihon-u.ac.jp>
2024-12-08 06:14:49 +0000

    fixed library

--- main.cpp
+++ main.cpp
@@ -2,24 +2,24 @@
 #include "LITE_EPS.h"
 LITE_EPS eps(PA_0,PA_4);
 RawSerial pc(USBTX,USBRX,9600);
-int main() {
-    int flag = 0; // condition
+int main(){
+    int flag = 0;
     float btvol;
     for(int i = 0; i < 100; i++) {
         //Get Battery Voltage
         eps.vol(&btvol);
         pc.printf("BatVol = %.2f [V]\r\n",btvol);
         //Power Saving Mode
-        if(btvol <= 3.5) {
+        if(btvol <= 3.5){
             eps.shut_down_regulator();
             flag = 1;
         } else {
             eps.turn_on_regulator();
             flag = 0;
         }
-        if(flag == 1) {
-            pc.printf("Power saving mode ON\r\n"); 
+        if(flag == 1){
+            pc.printf("Power saving mode ON\r\n");
         }
-        wait(1.0);
+        wait_ms(1000);
     }
 }
